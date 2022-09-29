020ac36378af307a8f75fcd1381c6bcdb85448c5
tonypingco <tonynguyen@pingco.com.au>
2022-09-29 23:51:08 +1000

    feat: add pages and router to app

---
+++ src/pages/HighScores.tsx
@@ -0,0 +1,7 @@
+export default function HighScores() {
+  return (
+    <div>
+      <h1>Highscores</h1>
+    </div>
+  );
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		port: 3000,
-	},
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      app: resolve(__dirname, "src", "app"),
+      components: resolve(__dirname, "src", "components"),
+      hooks: resolve(__dirname, "src", "hooks"),
+    },
+  },
 });
