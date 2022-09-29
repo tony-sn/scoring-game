26334e607fb16befe7fc350c110d69e858149a6f
tonypingco <tonynguyen@pingco.com.au>
2022-09-30 00:01:27 +1000

    fix: incorrect path at Home page

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
       app: resolve(__dirname, "src", "app"),
       components: resolve(__dirname, "src", "components"),
       hooks: resolve(__dirname, "src", "hooks"),
+      pages: resolve(__dirname, "src", "pages"),
     },
   },
 });
