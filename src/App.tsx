8d5dd0017d80ba0440baabed8aab8e5657d674a5
tonypingco <tonynguyen@pingco.com.au>
2022-09-29 23:49:18 +1000

    feat: add pages and router to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import './App.css';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+import Home from "pages/Home";
+import HighScores from "pages/HighScores";
+import Game from "pages/Game";
+import GameOver from "pages/GameOver";
 
 function App() {
-	return (
-		<div className="App">
-			<h1>Hello World</h1>
-		</div>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/highscores" element={<HighScores />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/gameover" element={<GameOver />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
