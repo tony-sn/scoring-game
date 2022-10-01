9da752dc197569519df4aa2a8d107e0e351fbc2e
tonypingco <tonynguyen@pingco.com.au>
2022-10-01 22:36:09 +1000

    style: add style to game page

--- src/styled/Container.tsx
+++ src/styled/Container.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  max-width: 1000px;
+  padding: 20px;
   margin: 0 auto;
   margin-top: 20px;
-  padding: 2rem;
+  max-width: 800px;
 `;
