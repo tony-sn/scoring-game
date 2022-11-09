2f312baba2f2ef844f49a65a4bc35527dee43e3a
tony-sn <tony.sn@outlook.com>
2022-11-09 23:47:20 +1100

    feat: add POST request gameOver

--- src/pages/GameOver.tsx
+++ src/pages/GameOver.tsx
@@ -1,24 +1,55 @@
+import { useState, useEffect } from "react";
+
 import { useScore } from "contexts/ScoreContext";
 import { StyledLink } from "styled/Navbar";
+import { StyledCharacter } from "styled/Game";
 
-import { NavigationProps } from "interfaces/index";
-import { useEffect } from "react";
+import { NavigationProps, Record } from "interfaces/index";
 
 export default function GameOver({ navigate }: NavigationProps): JSX.Element {
   const [score] = useScore();
+  const [scoreMessage, setScoreMessage] = useState("");
 
   useEffect(() => {
     if (score === -1) navigate("/"); // if not play, return to home page
     return;
   }, []);
 
+  useEffect(() => {
+    const saveHighScore = async () => {
+      try {
+        const options = {
+          method: "POST",
+          body: JSON.stringify({ name: "James", score }), // NOTE: ES6 shorthand
+        };
+        const res = await fetch("/.netlify/functions/saveHighScore", options);
+        const data: Record = await res.json();
+        if (data.id) {
+          setScoreMessage("Congrats! You got a high score!");
+        } else {
+          setScoreMessage("Sorry, not a high score. Keep trying!");
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    saveHighScore();
+  }, [score]);
+
   return (
     <div>
       <h1>Game Over</h1>
-      <p>{score}</p>
+      <h2>{scoreMessage}</h2>
+
+      <StyledCharacter>{score}</StyledCharacter>
+
       <br />
-      <StyledLink to="/">Go Home</StyledLink>
-      <StyledLink to="/game">Play Again?</StyledLink>
+      <div>
+        <StyledLink to="/">Go Home</StyledLink>
+      </div>
+      <div>
+        <StyledLink to="/game">Play Again?</StyledLink>
+      </div>
     </div>
   );
 }
