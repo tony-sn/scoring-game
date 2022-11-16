73fd5088ba6b404a33ab6a75eb192f8f59605dbb
tony-sn <tony.sn@outlook.com>
2022-11-16 23:56:15 +1100

    wip: validate access token

--- functions/utils/auth.ts
+++ functions/utils/auth.ts
@@ -3,6 +3,15 @@ interface EventHeaders {
   authorization: string;
 }
 
+const jwt = require("jsonwebtoken");
+const jwks = require("jwks-rsa");
+const jwksClient = jwks({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`,
+  audience: process.env.AUTH0_AUDIENCE,
+});
+
+const { promisify } = require("util");
+
 export const getAccessTokenFromHeaders = (headers: Partial<EventHeaders>) => {
   const rawAuthorization = headers.authorization;
   if (!rawAuthorization) {
@@ -15,4 +24,15 @@ export const getAccessTokenFromHeaders = (headers: Partial<EventHeaders>) => {
   }
   const accessToken = authorizationParts[1];
   return accessToken;
+};
+
+const validateAccessToken = (token) => {
+  try {
+    const decodedToken = jwt.decode(token, { complete: true });
+    const kid = decodedToken.header.kid;
+    const getSigningKey = promisify(jwksClient.getSigningKey);
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
