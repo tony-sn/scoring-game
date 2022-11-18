f561a4ed7e8149023334f94aee943798f0552337
tony-sn <tony.sn@outlook.com>
2022-11-18 23:37:01 +1100

    style: add standard style for pages

--- src/styled/Container.tsx
+++ src/styled/Container.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   padding: 20px;
   margin: 0 auto;
-  margin-top: 20px;
+  /* margin-top: 20px; */
   max-width: 800px;
   text-align: center;
 `;
--- src/styled/GlobalStyle.tsx
+++ src/styled/GlobalStyle.tsx
@@ -1,9 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 
+const isDarkTheme = false;
+
 export default createGlobalStyle`
+:root {
+  --main-bg-color: ${isDarkTheme ? "#333" : "#f9f9f9"};
+  --main-text-color: ${isDarkTheme ? "#f9f9f9" : "#333"};
+  --accent-color: #e16365;
+}
+
   *{
     box-sizing: border-box;
-    color: #333;
+    color: var(--main-text-color);
     margin: 0;
     font-family: sans-serif;
     font-weight: 300;
