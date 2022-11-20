229b4d86dcfed16012615f72ea952d330330ceac
tony-sn <tony.sn@outlook.com>
2022-11-20 15:37:46 +1100

    feat: save theme to localStorage

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -3,11 +3,18 @@ import React, { useState, useEffect } from "react";
 export function useTheme() {
   const [theme, setTheme] = useState("light");
 
+  useEffect(() => {
+    const localStorageTheme = window.localStorage.getItem("theme");
+    setTheme(localStorageTheme || "light");
+  }, []);
+
   const toggleTheme = () => {
     if (theme === "dark") {
       setTheme("light");
+      window.localStorage.setItem("theme", "light");
     } else {
       setTheme("dark");
+      window.localStorage.setItem("theme", "dark");
     }
   };
 
